b40c5c05f143869b8050da075db433d29602d4a2
poldinik <lorenzo.vannucchi1993@gmail.com>
2022-11-17 10:43:21 +0100

    Remove useless tests

--- src/test/java/it/gdg/pt/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package it.gdg.pt;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
