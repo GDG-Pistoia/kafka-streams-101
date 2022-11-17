e56cf9eb2ff92dcd7231b5d2164cc1fcae1f36c8
poldinik <lorenzo.vannucchi1993@gmail.com>
2022-11-17 12:19:27 +0100

    Format ln ret entity

--- src/main/java/it/gdg/pt/entities/LogarithmicReturn.java
+++ src/main/java/it/gdg/pt/entities/LogarithmicReturn.java
@@ -1,5 +1,6 @@
 package it.gdg.pt.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.*;
@@ -19,10 +20,14 @@ public class LogarithmicReturn {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private Double prev;
     private Double next;
     private Double logReturn;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime emitted_at;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime inserted_at;
 }
